5dc4d347f590d1341ccad0063dd2e766370e59d7
aquamarissa <vremapriklucenij16@gmail.com>
2019-10-10 22:17:30 +0200

    Squadnotprymokutnyk

--- UI/MapWindow.py
+++ UI/MapWindow.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from demo_1 import *
+from demo_2 import *
 from demo_1_python import *
 import math
 import numpy as np
@@ -47,35 +47,50 @@ class MapWindow(QDialog):
         self.heights = []
         self.count = -1
         self.focus = 0.00
+        self.height_cam = 0.00
+        self.weight_cam = 0.00
         self.height_point = Height()
+        self.map = ''
+        self.parties_h = 800
+        self.parties_w = 1250
+        self.height_poly = []
+        self.weight_poly = []
 
     def createWindow(self):
-        self.setGeometry(250, 250, 741, 451)
+        self.setGeometry(250, 250, 1250, 800)
         self.setWindowTitle("Мапа")
         self.show()
 
     def mousePressEvent(self, event):
-        print(self.focus)
         self.count += 1
+        self.height_point.createWindow()
         self.points.append(np.array([event.pos().x(), event.pos().y(), 0]))
         self.rects.append(self.focusArea(self.count))
-        if self.count > 0:
-            self.ways[0] = int((np.linalg.norm(self.points[self.count] - self.points[0]) + self.focus) /
-                               self.focus)
-            self.ways.append(int((np.linalg.norm(self.points[self.count - 1] - self.points[self.count]) + self.focus) /
-                                 self.focus))
-        self.height_point.createWindow()
         if self.count > 0:
             self.heights.append(self.height_point.height)
         else:
             self.heights.append(0)
+        self.weight_poly.append(((self.height_cam / 1000) * self.heights[self.count] / (self.focus / 1000))
+                                * self.parties_h)
+        #self.height_poly.append(((self.weight_cam / 1000) * self.heights[self.count] / (self.focus / 1000))
+         #                       * self.parties_w)
+        self.height_poly.append(30)
+        #self.weight_poly.append(50)
+        print(self.height_poly[self.count])
+        print(self.weight_poly[self.count])
+        if self.count > 0:
+            self.ways[0] = int((np.linalg.norm(self.points[self.count] - self.points[0])
+                                + self.weight_poly[self.count]) / self.weight_poly[self.count])
+            self.ways.append(int((np.linalg.norm(self.points[self.count - 1] - self.points[self.count])
+                                  + self.weight_poly[self.count]) / self.weight_poly[self.count]))
+        print(self.ways[self.count])
 
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
         pixmap = QtGui.QPixmap()
-        pixmap.load('images/mapa.jpeg')
-        painter.drawPixmap(0, 0, 741, 451, pixmap)
+        pixmap.load(self.map)
+        painter.drawPixmap(0, 0, 1250, 800, pixmap)
         painter.setPen(QPen(Qt.gray, 5, Qt.SolidLine))
         for i in range(len(self.points)):
             painter.drawEllipse(self.points[i][0], self.points[i][1], 8, 8)
@@ -86,10 +101,11 @@ class MapWindow(QDialog):
             if i > 0:
                 painter.setBrush(QtGui.QBrush(QtGui.QColor(55, 60, 61, 100)))
                 for j in range(self.ways[i]):
-                    temp_point = self.findStepLine(self.points[i - 1], self.points[i], j)
+                    temp_point = self.findStepLine(self.points[i - 1], self.points[i], j, self.weight_poly[i])
                     painter.drawPolygon(
-                        self.createPolygon(4, (math.sqrt(2 * self.focus ** 2) / 2),  # радіус рівний половині діагоналі
-                                           self.angels[i], temp_point[0], temp_point[1]))
+                        self.createPolygon(4, math.sqrt(self.weight_poly[i] ** 2 + self.weight_poly[i] ** 2) / 2,  # радіус рівний половині діагоналі
+                                           self.angels[i], temp_point[0], temp_point[1],
+                                           self.weight_poly[i]))
 
         painter.end()
         self.update()
@@ -105,8 +121,6 @@ class MapWindow(QDialog):
             mod_bc = np.linalg.norm(c - b)
             scalar_ba_bc = np.dot(vector_ba, vector_bc)
             cos_b = scalar_ba_bc / (mod_ba * mod_bc)
-            print(self.angels[len(self.angels)-1])
-            print(math.degrees(math.acos(cos_b)) + self.angels[len(self.angels)-1])
             self.angels.append(-math.degrees(math.acos(cos_b)) + self.angels[len(self.angels)-1])
             #return self.findPolygonPoints(i)
         elif i == 0:
@@ -121,27 +135,44 @@ class MapWindow(QDialog):
             mod_bc = np.linalg.norm(c - b)
             scalar_ba_bc = np.dot(vector_ba, vector_bc)
             cos_b = scalar_ba_bc / (mod_ba * mod_bc)
-            print(self.angels[len(self.angels) - 1])
-            print(math.degrees(math.acos(cos_b)) + self.angels[len(self.angels) - 1])
             self.angels.append(-math.degrees(math.acos(cos_b)) + self.angels[len(self.angels) - 1])
 
-    def findPolygonPoints(self, i):
-        f = self.focus
-        polygon = QRect(self.points[i][1] - f / 2, self.points[i][2] - f / 2, f, f)
-        return polygon
-
-    def findStepLine(self, point1, point2, step): # points[i - 1], points[i]
+    def findStepLine(self, point1, point2, count, step):  # points[i - 1], points[i]
         len = np.linalg.norm(point1 - point2)
-        point3 = point1 + (point2 - point1) * ((step * self.focus) / len)
+        print('len')
+        print(len)
+        print('step')
+        print(count)
+        point3 = point1 + (point2 - point1) * ((count * step) / len)
         return point3
 
-    def createPolygon(self, n, r, s, x_coord, y_coord):
+    def createPolygon(self, n, r, s, x_coord, y_coord, weight):  # ОБЕРЕЖНО ГОВНОКОД!!!!!!1!!!!!1!!                           (шучу він скрізь)
         polygon = QtGui.QPolygonF()
+        point = []
         w = 360 / n
-        for i in range(n):
-            t = w * i + s+45
-            x = r * math.cos(math.radians(t))
-            y = r * math.sin(math.radians(t))
-            polygon.append(QtCore.QPointF(x_coord + x, y_coord + y))
 
+        t1 = w * 0 + s + 45
+        x1 = r * math.cos(math.radians(t1))
+        y1 = r * math.sin(math.radians(t1))
+        point.append(np.array([x1, y1, 0]))
+
+        t2 = w * 1 + s + 45
+        x2 = r * math.cos(math.radians(t2))
+        y2 = r * math.sin(math.radians(t2))
+        point.append(np.array([x2, y2, 0]))
+
+        t3 = w * 2 + s + 45
+        x3 = r * math.cos(math.radians(t3))
+        y3 = r * math.sin(math.radians(t3))
+        point.append(np.array([x3, y3, 0]))
+
+        t4 = w * 3 + s + 45
+        x4 = r * math.cos(math.radians(t4))
+        y4 = r * math.sin(math.radians(t4))
+        point.append(np.array([x4, y4, 0]))
+
+        polygon.append(QtCore.QPointF(x_coord + x1, y_coord))
+        polygon.append(QtCore.QPointF(x_coord + x2, y_coord + y2))
+        polygon.append(QtCore.QPointF(x_coord + x3, y_coord + y3))
+        polygon.append(QtCore.QPointF(x_coord + x4, y_coord + y4))
         return polygon
--- UI/demo_1_python.py
+++ UI/demo_1_python.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import QDialog, QLabel
 from qtconsole.mainwindow import MainWindow
 
-from demo_1 import *
+from demo_2 import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -21,15 +21,19 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.image_window = MapWindow()
-        self.ui.pushButton.clicked.connect(self.setDepthLength)
+        self.ui.pushButton.clicked.connect(self.setAllOptions)
         self.ui.pushButton.clicked.connect(self.image_window.createWindow)
+        self.ui.pushButton_2.clicked.connect(self.selectFile)
 
-    def setDepthLength(self):
-        if self.ui.lineEdit_2.text():
-            depth_length = float(self.ui.lineEdit_2.text())
-            self.image_window.focus = depth_length
-        else:
-            return 0
+    def selectFile(self):
+        self.image_window.map = ''.join(QFileDialog.getOpenFileName()[0])
+
+    def setAllOptions(self):
+        self.image_window.focus = float(self.ui.lineEdit_2.text())
+        self.image_window.parties_h = self.image_window.parties_h / int(self.ui.lineEdit_8.text())
+        self.image_window.parties_w = self.image_window.parties_w / int(self.ui.lineEdit_9.text())
+        self.image_window.height_cam = float(self.ui.lineEdit_3.text())
+        self.image_window.weight_cam = float(self.ui.lineEdit_6.text())
 
 
 if __name__ == "__main__":
@@ -39,3 +43,5 @@ if __name__ == "__main__":
     height_window = Height()
     myapp.show()
     sys.exit(app.exec_())
+
+ # Посланіє: треба перенести усі значення з камери у клас мапи, а потім для кожної точки та її висоти визначати розміри
