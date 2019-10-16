a769636834006eda9c82e35315dcb85c7c3bc1eb
aquamarissa <vremapriklucenij16@gmail.com>
2019-10-16 22:21:31 +0200

    Konec

--- UI/demo_1_python.py
+++ UI/demo_1_python.py
@@ -18,22 +18,85 @@ from MapWindow import *
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
+        self.start_acces = [True] * 10
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.image_window = MapWindow()
         self.ui.pushButton.clicked.connect(self.setAllOptions)
-        self.ui.pushButton.clicked.connect(self.image_window.createWindow)
         self.ui.pushButton_2.clicked.connect(self.selectFile)
 
     def selectFile(self):
         self.image_window.map = ''.join(QFileDialog.getOpenFileName()[0])
 
     def setAllOptions(self):
-        self.image_window.focus = float(self.ui.lineEdit_2.text())
-        self.image_window.parties_h = self.image_window.parties_h / int(self.ui.lineEdit_8.text())
-        self.image_window.parties_w = self.image_window.parties_w / int(self.ui.lineEdit_9.text())
-        self.image_window.height_cam = float(self.ui.lineEdit_3.text())
-        self.image_window.weight_cam = float(self.ui.lineEdit_6.text())
+        self.image_window.acces = True
+        if self.image_window.map == '':
+            self.start_acces[0] = False
+        else:
+            self.start_acces[0] = True
+
+        if float(self.ui.lineEdit.text()) < 0:
+            self.start_acces[1] = False
+        else:
+            self.image_window.height_point.max_height = float(self.ui.lineEdit.text())
+            self.start_acces[1] = True
+
+        if float(self.ui.lineEdit_2.text()) < 0:
+            self.start_acces[2] = False
+        else:
+            self.image_window.focus = float(self.ui.lineEdit_2.text())
+            self.start_acces[2] = True
+
+        if int(self.ui.lineEdit_8.text()) < 0:
+            self.start_acces[3] = False
+        else:
+            self.image_window.parties_h = self.image_window.parties_h / int(self.ui.lineEdit_8.text())
+            self.start_acces[3] = True
+
+        if int(self.ui.lineEdit_9.text()) < 0:
+            self.start_acces[4] = False
+        else:
+            self.image_window.parties_w = self.image_window.parties_w / int(self.ui.lineEdit_9.text())
+            self.start_acces[4] = True
+
+        if float(self.ui.lineEdit_3.text()) < 0:
+            self.start_acces[5] = False
+        else:
+            self.image_window.height_cam = float(self.ui.lineEdit_3.text())
+            self.start_acces[5] = True
+
+        if float(self.ui.lineEdit_6.text()) < 0:
+            self.start_acces[6] = False
+        else:
+            self.image_window.weight_cam = float(self.ui.lineEdit_6.text())
+            self.start_acces[6] = True
+
+        if float(self.ui.lineEdit_7.text()) < 0 or float(self.ui.lineEdit_7.text()) > 100:
+            self.start_acces[7] = False
+        else:
+            self.image_window.percent_start = float(self.ui.lineEdit_7.text())
+            self.start_acces[7] = True
+
+        if float(self.ui.lineEdit_5.text()) < 0 or float(self.ui.lineEdit_5.text()) > 100:
+            self.start_acces[8] = False
+        else:
+            self.image_window.percent_per_hundred = float(self.ui.lineEdit_5.text())
+            self.start_acces[8] = True
+
+        if float(self.ui.lineEdit_4.text()) < 0 or float(self.ui.lineEdit_4.text()) > 100:
+            self.start_acces[9] = False
+        else:
+            self.image_window.percent_per_photo = float(self.ui.lineEdit_4.text())
+            self.start_acces[9] = True
+
+        for i in self.start_acces:
+            if not i:
+                self.image_window.acces = False
+
+        self.image_window.createWindow()
+
+    def callError(self, message):
+        QMessageBox.about(self, "Помилка", message)
 
 
 if __name__ == "__main__":
@@ -43,5 +106,3 @@ if __name__ == "__main__":
     height_window = Height()
     myapp.show()
     sys.exit(app.exec_())
-
- # Посланіє: треба перенести усі значення з камери у клас мапи, а потім для кожної точки та її висоти визначати розміри
